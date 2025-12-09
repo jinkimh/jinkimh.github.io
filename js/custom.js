aaa450aff045dd4aa361501a2ef054d7a1333586
Jin H Kim <jin.kim@gnu.ac.kr>
2025-12-09 21:31:02 +0900

    Complete navigation improvements: accessibility, mobile UX, design consistency, and dropdown toggle

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,143 @@
 // Custom Script
 // Developed by: Samson.Onna
 var customScripts = {
+    navigation: function () {
+        // 햄버거 버튼 ARIA 상태 업데이트
+        $('#nav-toggle').on('click', function() {
+            var isExpanded = $(this).attr('aria-expanded') === 'true';
+            $(this).attr('aria-expanded', !isExpanded);
+        });
+        
+        // Bootstrap collapse 이벤트와 동기화
+        $('#main-nav').on('show.bs.collapse', function() {
+            $('#nav-toggle').attr('aria-expanded', 'true');
+        }).on('hide.bs.collapse', function() {
+            $('#nav-toggle').attr('aria-expanded', 'false');
+            // 메뉴가 닫힐 때 모든 드롭다운도 닫기
+            $('.has-dropdown').removeClass('open').attr('aria-expanded', 'false');
+        });
+        
+        // 데스크톱: 드롭다운 클릭 지원
+        var isMobile = $(window).width() <= 767;
+        
+        function handleDropdown() {
+            isMobile = $(window).width() <= 767;
+            
+            // 기존 이벤트 리스너 제거
+            $('.has-dropdown > a.dropdown-toggle').off('click.dropdown keydown.dropdown');
+            
+            if (isMobile) {
+                // 모바일: 클릭으로 토글
+                $('.has-dropdown > a.dropdown-toggle').on('click.dropdown', function(e) {
+                    e.preventDefault();
+                    var $parent = $(this).parent('.has-dropdown');
+                    var $dropdown = $parent.find('.dropdown');
+                    var isOpen = $parent.hasClass('open');
+                    
+                    // 다른 드롭다운 닫기
+                    $('.has-dropdown').not($parent).removeClass('open').attr('aria-expanded', 'false')
+                        .find('.dropdown').css('max-height', '0');
+                    
+                    // 현재 드롭다운 토글
+                    if (isOpen) {
+                        $parent.removeClass('open').attr('aria-expanded', 'false');
+                        $dropdown.css('max-height', '0');
+                    } else {
+                        $parent.addClass('open').attr('aria-expanded', 'true');
+                        $dropdown.css('max-height', '500px');
+                    }
+                });
+            } else {
+                // 데스크톱: 클릭으로도 토글 가능 (hover는 CSS로 처리)
+                $('.has-dropdown > a.dropdown-toggle').on('click.dropdown', function(e) {
+                    e.preventDefault();
+                    var $parent = $(this).parent('.has-dropdown');
+                    var isOpen = $parent.hasClass('open');
+                    
+                    // 다른 드롭다운 닫기
+                    $('.has-dropdown').not($parent).removeClass('open').attr('aria-expanded', 'false');
+                    
+                    // 현재 드롭다운 토글
+                    if (isOpen) {
+                        $parent.removeClass('open').attr('aria-expanded', 'false');
+                    } else {
+                        $parent.addClass('open').attr('aria-expanded', 'true');
+                    }
+                });
+            }
+            
+            // 키보드 네비게이션 (모바일/데스크톱 공통)
+            $('.has-dropdown > a.dropdown-toggle').on('keydown.dropdown', function(e) {
+                var $parent = $(this).parent('.has-dropdown');
+                var $dropdown = $parent.find('.dropdown');
+                
+                // Enter 또는 Space로 토글
+                if (e.key === 'Enter' || e.key === ' ') {
+                    e.preventDefault();
+                    var isOpen = $parent.hasClass('open');
+                    
+                    // 다른 드롭다운 닫기
+                    $('.has-dropdown').not($parent).removeClass('open').attr('aria-expanded', 'false');
+                    if (isMobile) {
+                        $('.has-dropdown').not($parent).find('.dropdown').css('max-height', '0');
+                    }
+                    
+                    // 현재 드롭다운 토글
+                    if (isOpen) {
+                        $parent.removeClass('open').attr('aria-expanded', 'false');
+                        if (isMobile) {
+                            $dropdown.css('max-height', '0');
+                        }
+                    } else {
+                        $parent.addClass('open').attr('aria-expanded', 'true');
+                        if (isMobile) {
+                            $dropdown.css('max-height', '500px');
+                        }
+                    }
+                }
+                // Escape로 드롭다운 닫기
+                else if (e.key === 'Escape') {
+                    $parent.removeClass('open').attr('aria-expanded', 'false');
+                    if (isMobile) {
+                        $dropdown.css('max-height', '0');
+                    }
+                }
+            });
+            
+            // 드롭다운 외부 클릭 시 닫기 (데스크톱만)
+            if (!isMobile) {
+                $(document).on('click.dropdown', function(e) {
+                    if (!$(e.target).closest('.has-dropdown').length) {
+                        $('.has-dropdown').removeClass('open').attr('aria-expanded', 'false');
+                    }
+                });
+            }
+        }
+        
+        // 초기화
+        handleDropdown();
+        
+        // 리사이즈 시 재초기화
+        $(window).on('resize', function() {
+            handleDropdown();
+        });
+        
+        // 메뉴 항목 클릭 시 모바일에서 메뉴 닫기
+        $('.navbar-inverse').on('click', 'li:not(.has-dropdown) > a', function () {                
+            if ($(window).width() <= 767) {
+                $('#main-nav').collapse('hide');
+            }
+        });
+        
+        // 드롭다운 항목 클릭 시 모바일에서 메뉴 닫기
+        $('.dropdown a').on('click', function() {
+            if ($(window).width() <= 767) {
+                $('#main-nav').collapse('hide');
+                $('.has-dropdown').removeClass('open').attr('aria-expanded', 'false')
+                    .find('.dropdown').css('max-height', '0');
+            }
+        });
+    },
     profile: function () {
         // portfolio
         if ($('.isotopeWrapper').length) {
@@ -91,6 +228,7 @@ var customScripts = {
         });
     },
     init: function () {
+        customScripts.navigation();
         customScripts.onePageNav();
         customScripts.profile();
         customScripts.fancybox();
